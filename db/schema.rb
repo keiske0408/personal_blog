77c1a58027c84d9ac8d3f261988919ef99edc85f
keiske0408 <kieske0408@gmail.com>
2024-10-06 01:25:59 +0800

    adding validations

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_10_05_165457) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
